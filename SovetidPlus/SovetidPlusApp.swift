07ac93a0fcef94c43db345737f10dbfc099570dc
Andjalis <andjalis@gmail.com>
2025-10-29 19:04:51 +0100

    Initial Commit

---
+++ SovetidPlus/SovetidPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SovetidPlusApp.swift
+//  SovetidPlus
+//
+//  Created by Andjalis on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SovetidPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SovetidPlusTests/SovetidPlusTests.swift
@@ -0,0 +1,17 @@
+//
+//  SovetidPlusTests.swift
+//  SovetidPlusTests
+//
+//  Created by Andjalis on 29/10/2025.
+//
+
+import Testing
+@testable import SovetidPlus
+
+struct SovetidPlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
